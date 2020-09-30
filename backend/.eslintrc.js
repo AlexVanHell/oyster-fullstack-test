7e398f7d30bcfbc1f86838aa05686bf4ad64b0b0
alexvanhell <alejandrodnl.cv@hotmail.com>
2020-09-30 18:04:09 -0500

    Project setup

---
+++ backend/.eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: './backend/tsconfig.json',
+		sourceType: 'module',
+		createDefaultProgram: true,
+	},
+	plugins: ['@typescript-eslint/eslint-plugin'],
+	extends: [
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier',
+		'prettier/@typescript-eslint',
+	],
+	root: true,
+	env: {
+		node: true,
+		jest: true,
+	},
+	rules: {
+		'@typescript-eslint/interface-name-prefix': 'off',
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/array-type': 'warn',
+		'@typescript-eslint/explicit-member-accessibility': [
+			'warn',
+			{
+				accessibility: 'explicit',
+				overrides: {
+					accessors: 'explicit',
+					constructors: 'no-public',
+					methods: 'explicit',
+					properties: 'off',
+					parameterProperties: 'explicit',
+				},
+			},
+		],
+		'@typescript-eslint/member-ordering': 'warn',
+	},
+	settings: {
+		'eslint.workingDirectories': [
+			{ directory: './tsconfig.json', changeProcessCWD: true },
+		],
+	},
+};
